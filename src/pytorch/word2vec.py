59c09da639bca556909d0e145760b9c0d44b67ab
hipster4020 <hipster4020@gmail.com>
2022-12-12 19:03:01 +0900

    word2vec 수정

--- src/pytorch/word2vec.py
+++ src/pytorch/word2vec.py
@@ -1,8 +1,12 @@
 import hydra
 
+# import wandb
+
 
 @hydra.main(config_name="config.yaml")
 def main(cfg) -> None:
+    # wandb.init(project=cfg.ETC.project)
+
     input_dim = cfg.MODEL.input_dim
     print(input_dim)
 
