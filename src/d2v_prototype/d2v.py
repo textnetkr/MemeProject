cd353b4f438dbd3cdbd5efda41c3310964bb6a8d
hipster4020 <hipster4020@gmail.com>
2023-01-10 21:54:29 +0900

    memebot 개선 작업

--- src/d2v_prototype/d2v.py
+++ src/d2v_prototype/d2v.py
@@ -1,3 +1,5 @@
+import base64
+
 import config as cfg
 import streamlit as st
 from gensim.models.doc2vec import Doc2Vec
@@ -46,47 +48,102 @@ def get_model():
 def main():
     df, df_ref, model, tokenizer = get_model()
 
+    # background image
+    def get_base64(bin_file):
+        with open(bin_file, "rb") as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    def set_background(png_file):
+        bin_str = get_base64(png_file)
+        page_bg_img = (
+            """
+            <style>
+            .stApp {
+            margin: 0 auto;
+            background-image: url("data:image/png;base64,%s");
+            background-size: cover;
+            background-color: #fbe100;
+            }
+            </style>
+            """
+            % bin_str
+        )
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+
+    set_background("bg.png")
+
     # title
-    st.title("doc2vec 유사도 기반 밈봇")
     st.header("")
     st.header("")
     st.header("")
+    st.header("")
+    st.header("")
+    st.header("")
+    st.header("")
+    st.title("밈 중독 대춘이를 만나보세요!")
+
     # image
+    # 너굴 이미지
     image = Image.open("bot.png")
     st.image(
         image,
         caption=None,
-        width=50,
+        width=20,
         use_column_width=None,
         clamp=False,
         channels="RGB",
         output_format="auto",
     )
-    # input text
-    input = st.text_input(
-        label="",
-        placeholder="대춘이에게 말을 걸어주세요!",
-    )
-    print(f"input : {input}")
 
-    if st.button("Enter"):
+    # 정렬
+    mystyle = """
+    <style>
+        p {
+            text-align: justify;
+        }
+    </style>
+    """
+    st.markdown(mystyle, unsafe_allow_html=True)
+
+    # Generate Two equal columns
+    c1, c2 = st.columns((1, 1))
+    c11, c22 = st.columns((1, 1))
+    c111, c222 = st.columns((1, 1))
+
+    # 대춘이
+    with c1:
         st.header("")
-        # vectorize new sentences based on doc2vec
-        input_vec = model.infer_vector(tokenizer.tokenize(input))
+        con1 = st.container()
+        con1.caption("🤖 대춘이")
+        con1.info("대춘이에게 말을 걸어주세요!")
 
-        # most similarity top 1
-        return_docs = model.docvecs.most_similar(positive=[input_vec], topn=1)
-        result_docs = df.u.iloc[int(return_docs[0][0])]
-        print(f"유사 문장 : {result_docs}")
+    # My Utterance
+    with c22:
+        # input text
+        input = st.text_input(label="🤗 대길이")
+        print(f"input : {input}")
+    with c111:
+        if input:
+            # vectorize new sentences based on doc2vec
+            input_vec = model.infer_vector(tokenizer.tokenize(input))
 
-        temp_ref = df_ref[df_ref.u.values == result_docs["u"]]
-        temp_ref = temp_ref.sample(frac=1).reset_index(drop=True)
-        result = str(temp_ref.iloc[0]["meme"])
+            # most similarity top 1
+            return_docs = model.docvecs.most_similar(
+                positive=[input_vec],
+                topn=1,
+            )
+            result_docs = df.u.iloc[int(return_docs[0][0])]
+            print(f"유사 문장 : {result_docs}")
 
-        # output
-        con = st.container()
-        con.caption("밈봇 답변")
-        st.success(str(result.strip()))
+            temp_ref = df_ref[df_ref.u.values == result_docs["u"]]
+            temp_ref = temp_ref.sample(frac=1).reset_index(drop=True)
+            result = str(temp_ref.iloc[0]["meme"])
+
+            # output
+            con2 = st.container()
+            con2.caption("🤖 대춘이")
+            st.info(str(result.strip()))
 
 
 if __name__ == "__main__":
