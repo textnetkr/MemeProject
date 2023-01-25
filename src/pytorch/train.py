e9335389f96d87fb3f0c6a3f3be0c501830776b0
hipster4020 <hipster4020@gmail.com>
2023-01-25 10:21:43 +0900

    train modified

---
+++ src/pytorch/train.py
@@ -0,0 +1,72 @@
+import hydra
+import numpy as np
+import wandb
+from dataloader import load
+from datasets import load_metric
+from pshmodule.utils import filemanager as fm
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+    default_data_collator,
+)
+
+
+@hydra.main(config_path="./", config_name="config")
+def main(cfg):
+    # tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(cfg.MODEL.name)
+
+    # data loder
+    train_dataset, eval_dataset = load(tokenizer=tokenizer, **cfg.DATASETS)
+
+    args = TrainingArguments(
+        **cfg.TRAININGS,
+    )
+
+    # wandb
+    wandb.init(
+        project=cfg.ETC.project,
+        entity=cfg.ETC.entity,
+        name=cfg.ETC.name,
+    )
+
+    # model
+    model = AutoModelForSequenceClassification.from_pretrained(
+        cfg.MODEL.name,
+        num_labels=fm.load(cfg.DATASETS.train_data_path)["label"].iloc[-1],
+    )
+
+    # metrics
+    def compute_metrics(eval_preds):
+        metric = load_metric(cfg.METRICS.metric_name)
+        logits, labels = eval_preds
+        predictions = np.argmax(logits, axis=-1)
+
+        return metric.compute(
+            predictions=predictions,
+            references=labels,
+            average=cfg.METRICS.average,
+        )
+
+    # train
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=default_data_collator,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+
+    # model save
+    trainer.save_model(cfg.PATH.save_dir)
+
+    if cfg.ETC.get("wandb_project"):
+        wandb.finish()
+
+
+if __name__ == "__main__":
+    main()
