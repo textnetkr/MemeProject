028d5cd3169ed8429ef1e9542b26b58110e88946
hipster4020 <hipster4020@gmail.com>
2023-02-20 18:13:08 +0900

    문체 변환 module화

--- src/pytorch/augmentation/dataloader.py
+++ src/pytorch/augmentation/dataloader.py
@@ -1,5 +1,7 @@
 from torch.utils.data import Dataset
-
+from os.path import abspath, splitext
+from datasets import load_dataset
+from typing import Optional
 
 style_map = {
     "formal": "문어체",
@@ -38,8 +40,15 @@ class TextStyleTransferDataset(Dataset):
 
     def __getitem__(self, index):
         row = self.df.iloc[index, :].dropna().sample(2)
+        # row = self.df.iloc[index, :]
+        # text1 = row[0]
+        # print(f"text1 : {text1}")
+        # row_sample = row.dropna().sample(2)
+        # text2 = row_sample[1]
+        # print(f"text2 : {text2}")
         text1 = row[0]
         text2 = row[1]
+
         target_style = row.index[1]
         target_style_name = style_map[target_style]
 
@@ -57,3 +66,76 @@ class TextStyleTransferDataset(Dataset):
         del model_inputs["token_type_ids"]
 
         return model_inputs
+
+
+def load(
+    tokenizer,
+    seq_len,
+    train_data_path: str,
+    eval_data_path: Optional[str] = None,
+    train_test_split: Optional[float] = None,
+    worker: int = 1,
+    batch_size: int = 1000,
+    shuffle_seed: Optional[int] = None,
+):
+    def _tokenize_function(e):
+        tokenized = dict()
+
+        tokenized = tokenizer(
+            e["content"],
+            max_length=seq_len,
+            padding="max_length",
+            truncation=True,
+            return_tensors="np",
+        )
+        print(f"e : {e}")
+        tokenized["labels"] = e["label"]
+
+        return tokenized
+
+    train_data_path = abspath(train_data_path)
+    is_eval = False
+    _, extention = splitext(train_data_path)
+
+    datafiles = {"train": train_data_path}
+    if eval_data_path is not None:
+        assert (
+            train_test_split is None
+        ), "Only one of eval_data_path and train_test_split must be entered."
+        datafiles["test"] = abspath(eval_data_path)
+        is_eval = True
+
+    if train_test_split is not None:
+        assert (
+            0.0 < train_test_split < 1.0
+        ), "train_test_split must be a value between 0 and 1"
+        train_test_split = int(train_test_split * 100)
+        train_test_split = {
+            "train": f"train[:{train_test_split}%]",
+            "test": f"train[{train_test_split}%:]",
+        }
+        is_eval = True
+
+    data = load_dataset(
+        extention.replace(".", ""),
+        data_files=datafiles,
+        split=train_test_split,
+    )
+
+    if shuffle_seed is not None:
+        data = data.shuffle(seed=shuffle_seed)
+
+    data = data.map(
+        _tokenize_function,
+        batched=True,
+        batch_size=batch_size,
+        num_proc=worker,
+        remove_columns=data["train"].column_names,
+    )
+
+    return data["train"], (data["test"] if is_eval else None)
+
+
+# Write preprocessor code to run in batches.
+def default_collator(data):
+    return data
