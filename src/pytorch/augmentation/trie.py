52b6613ab9c4e5e8d1f5e9a1b161c8c19ae49528
hipster4020 <hipster4020@gmail.com>
2023-03-27 21:24:47 +0900

    파일 수정

---
+++ src/pytorch/augmentation/trie.py
@@ -0,0 +1,59 @@
+# trie 자료 구조
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key  # 값으로 입력할 문자
+        self.data = data  # 문자열의 종료를 알리는 flag
+        self.children = {}  # 자식 노드
+
+
+class Trie:
+    def __init__(self):  # head 빈 노드 설정
+        self.head = Node(None)
+
+    def insert(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+
+    def search(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+
+        if current_node.data:
+            return True
+        else:
+            return False
+
+    def starts_with(self, prefix):
+        current_node = self.head
+        words = []
+
+        for p in prefix:
+            if p in current_node.children:
+                current_node = current_node.children[p]
+            else:
+                return None
+
+        current_node = [current_node]
+        next_node = []
+        while True:
+            for node in current_node:
+                if node.data:
+                    words.append(node.data)
+                next_node.extend(list(node.children.values()))
+            if len(next_node) != 0:
+                current_node = next_node
+                next_node = []
+            else:
+                break
+
+        return words
